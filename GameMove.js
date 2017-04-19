d0754325619dd6023e6b0f74719a9af9a8d5a1c0
Jorge Moreno <jorge.a.moreno.personal@gmail.com>
2017-04-19 22:48:12 +0000

    my master piece

--- GameMove.js
+++ GameMove.js
@@ -5,9 +5,9 @@ $(document).ready(function(){
        
         setInterval(function(){
             
-            console.log($("#ground").offset().top);
-            if( $("#ground").offset().top >   $("#fire").offset().top ){
-                    console.log($("#ground").offset().top);
+            //console.log($("#fire").offset().top);
+            if( $("#ground").offset().top >   $("#fire").offset().top + $("#fire").height()){
+                   // console.log($("#ground").offset().top);
 
                    $("#fire").css("top", $("#fire").offset().top + 40 ); 
 
@@ -18,4 +18,37 @@ $(document).ready(function(){
             
             }, 100);
             
+$("body").keydown(function( event ) {
+      if (event.which === 37 && $("#snowman").offset().left > $("#ground").offset().left){
+        $("#snowman").css("left", $("#snowman").offset().left - 35);
+   
+    }
+    
+    
+    else if (event.which === 39 && $("#snowman").offset().left < ($("#ground").width()  - $("#snowman").width())) 
+    {
+       //console.log( $("#ground").width())
+       console.log( $("#snowman").width())
+       //console.log($("#snowman").offset().left)
+
+        $("#snowman").css("left", $("#snowman").offset().left + 35);
+    } 
+    else{
+        return;
+    
+    }
+
 });
+
+ 
+   
+  
+});
+
+
+
+//function checkCollision(){
+    
+    //var snowmove =$("#snowman").offset().top;
+    
+//}

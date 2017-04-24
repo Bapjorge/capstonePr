0702ca1a61b5ecae118d15dbd2a6aadeed2d6181
Jorge Moreno <jorge.a.moreno.personal@gmail.com>
2017-04-24 22:43:37 +0000

    s

--- GameMove.js
+++ GameMove.js
@@ -2,21 +2,23 @@
 
 $(document).ready(function(){
        var $pic = $("#pic3"); 
+       var w = window.innerWidth - $("#fire").width();
        
         setInterval(function(){
             
-            //console.log($("#fire").offset().top);
+        
             if( $("#ground").offset().top >   $("#fire").offset().top + $("#fire").height()){
-                   // console.log($("#ground").offset().top);
+               
 
                    $("#fire").css("top", $("#fire").offset().top + 40 ); 
 
                 }
                 else {
-                    $("#fire").css("top", 0 ); 
+                    $("#fire").css("top", 5 ); $("#fire").css("left", Math.random()*w );
                 }
             
-            }, 100);
+            }, 75);
+            
             
 $("body").keydown(function( event ) {
       if (event.which === 37 && $("#snowman").offset().left > $("#ground").offset().left){
@@ -27,9 +29,7 @@ $("body").keydown(function( event ) {
     
     else if (event.which === 39 && $("#snowman").offset().left < ($("#ground").width()  - $("#snowman").width())) 
     {
-       //console.log( $("#ground").width())
-       console.log( $("#snowman").width())
-       //console.log($("#snowman").offset().left)
+     
 
         $("#snowman").css("left", $("#snowman").offset().left + 35);
     } 

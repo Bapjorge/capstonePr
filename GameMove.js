2282a3b4a0ef0060fcf49876c7084f51b5c220e5
Jorge Moreno <jorge.a.moreno.personal@gmail.com>
2017-04-26 22:42:50 +0000

    woo

--- GameMove.js
+++ GameMove.js
@@ -1,10 +1,17 @@
 // JavaScript File
-
+function checkCollision(){
+    if($("#snowman").offset().top <  $("#fire").offset().top  + $("#fire").height()){
+    
+     $("#snowman").hide();
+     alert("GAME OVER");}
+}
 $(document).ready(function(){
        var $pic = $("#pic3"); 
        var w = window.innerWidth - $("#fire").width();
+       var score = 0;
        
-        setInterval(function(){
+        setInterval(function(){   
+            checkCollision();
             
         
             if( $("#ground").offset().top >   $("#fire").offset().top + $("#fire").height()){
@@ -12,14 +19,18 @@ $(document).ready(function(){
 
                    $("#fire").css("top", $("#fire").offset().top + 40 ); 
 
-                }
+                } 
                 else {
-                    $("#fire").css("top", 5 ); $("#fire").css("left", Math.random()*w );
+                    $("#fire").css("top", 6 ); $("#fire").css("left", Math.random()*w );
+                    $("#box3").html(score += 1); 
+                    console.log(score);
                 }
             
             }, 75);
             
             
+            
+
 $("body").keydown(function( event ) {
       if (event.which === 37 && $("#snowman").offset().left > $("#ground").offset().left){
         $("#snowman").css("left", $("#snowman").offset().left - 35);
